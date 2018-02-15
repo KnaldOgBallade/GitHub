3129e722fb73d8b06747d8d4ee621079429d43a7
KnaldOgBallade <36263773+KnaldOgBallade@users.noreply.github.com>
2018-02-15 12:09:00 +0100

    asda

--- js/navApp.js
+++ js/navApp.js
@@ -3,16 +3,47 @@ function navigate(dest) {
     alert("You're being directed to " + dest + ". Please go to nearest beacon to get directions");
 }
 
-function colochange(color){
-  var tester;
-    if (color == 1){
-      tester = '#fff';
-    }else{
-      tester = 'powderblue';
-    }
-
-
+function onLoad(){
+	document.addEventListener('deviceready', onDeviceReady, false);
+    bleDeviceList.addEventListener('touchstart', conn, false); // assume not scrolling
 }
+
+function onDeviceReady(){
+	refreshDeviceList();
+}
+
+function refreshDeviceList(){
+	//deviceList =[];
+	document.getElementById("bleDeviceList").innerHTML = ''; // empties the list
+	if (cordova.platformId === 'android') { // Android filtering is broken
+		ble.scan([], 5, onDiscoverDevice, onError);
+	} else {
+		//alert("Disconnected");
+		ble.scan([blue.serviceUUID], 5, onDiscoverDevice, onError);
+	}
+}
+
+function myFunction() {
+    document.body.style.backgroundColor = "red";
+}
+
+function onDiscoverDevice(device){
+	//Make a list in html and show devises
+	var listItem = document.createElement('li'),
+    html = device.name+ "," + device.id;
+    listItem.innerHTML = html;
+    document.getElementById("bleDeviceList").appendChild(listItem);
+    device.id="test";
+    if( device.id == "test" ){
+      myFunction();
+    }
+}
+
+
+function onError(reason)  {
+	alert("ERROR: " + reason); // real apps should use notification.alert
+}
+
 
 /* DROP-DOWN MENU:
 When the user clicks on the button,
